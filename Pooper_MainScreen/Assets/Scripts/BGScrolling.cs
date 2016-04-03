53b475447ca86f5f7594ec47c1afbfc210728633
MANUELA REIS <manu.mreis@hotmail.com>
2016-04-03 15:55:23 -0300

    criacao do arquivo Pooper_MainScreen/

---
+++ Pooper_MainScreen/Assets/Scripts/BGScrolling.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGScrolling : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+}
---
+++ Pooper_MainScreen/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float maxSpeed = 10f;
+	 Rigidbody2D rb;
+
+	Animator anim;
+
+	bool grounded = false;
+	public Transform groundChek;
+	float groundRadius = 0.2f;
+	public LayerMask whatIsGround;
+	public float jumpForce = 75.0f;
+	public float forwardMovementSpeed = 3f;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+	}
+
+	void Update(){
+	}
+
+	void FixedUpdate () {
+
+		bool jumpActive = Input.GetButton ("Fire1");
+
+		if (jumpActive) {
+			rb.AddForce(new Vector2(0, jumpForce));
+		}
+
+		grounded = Physics2D.OverlapCircle (groundChek.position, groundRadius, whatIsGround);
+		anim.SetBool ("Ground", grounded);
+
+		/*Vector2 newVelocity = rb.velocity;
+		newVelocity.x = forwardMovementSpeed;
+		rb.velocity = newVelocity;
+         * */
+
+	}
+
+}
